15d9515f9500ce9182bea37a2edae25d6539bbc0
LaraBuiting <l.buiting@sintlucasedu.nl>
2023-12-05 14:59:45 +0100

    started on can script

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class playerMovement : MonoBehaviour
@@ -12,15 +13,19 @@ public class playerMovement : MonoBehaviour
         float inputX = Input.GetAxis("Horizontal");
         float inputY = Input.GetAxis("Vertical");
 
-
+        //If you press the basic sideways buttons the character moves sideways
         if (Input.GetKey(KeyCode.A) ^ Input.GetKey(KeyCode.D) ^ Input.GetKey(KeyCode.RightArrow) ^ Input.GetKey(KeyCode.LeftArrow))
         {
             transform.Translate(Vector2.right * inputX * speed * Time.deltaTime);
         }
 
+        //If you press the basis up down buttons the character moves that way
         if (Input.GetKey(KeyCode.W) ^ Input.GetKey(KeyCode.S) ^ Input.GetKey(KeyCode.UpArrow) ^ Input.GetKey(KeyCode.DownArrow))
         {
             transform.Translate(Vector2.up * inputY * speed * Time.deltaTime);
         }
     }
+
+    //If you collide with the energy drink your speed gets faster for a little bit and your energy bar goes up you also get a 
+    //empty energy drink can in your inventory which you have to delever to a trash can.
 }
