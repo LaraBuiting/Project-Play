592a8e8fd2bdac02a7b263002b5e0c16f791bb5d
LaraBuiting <l.buiting@sintlucasedu.nl>
2023-12-12 16:03:01 +0100

    Worked on some of the drink mechanics

--- Assets/Scripts/Player/playerMovement.cs
+++ Assets/Scripts/Player/playerMovement.cs
@@ -2,10 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class playerMovement : MonoBehaviour
 {
+    public float normalSpeed = 2.5f;
+    public float energySpeed = 3.5f;
     public float speed = 2.5f;
+
+    private bool Cooldown = false;
 
     // Update is called once per frame
     void Update()
@@ -17,15 +22,37 @@ public class playerMovement : MonoBehaviour
         if (Input.GetKey(KeyCode.A) ^ Input.GetKey(KeyCode.D) ^ Input.GetKey(KeyCode.RightArrow) ^ Input.GetKey(KeyCode.LeftArrow))
         {
             transform.Translate(Vector2.right * inputX * speed * Time.deltaTime);
+
+            //If you press E then you gain speed because you drank some energy
+            if (Input.GetKey(KeyCode.E) && Cooldown == false)
+            {
+                speed = energySpeed;
+                StartCoroutine(waitAFewSeconds(6f));
+            }
         }
+
 
         //If you press the basis up down buttons the character moves that way
         if (Input.GetKey(KeyCode.W) ^ Input.GetKey(KeyCode.S) ^ Input.GetKey(KeyCode.UpArrow) ^ Input.GetKey(KeyCode.DownArrow))
         {
             transform.Translate(Vector2.up * inputY * speed * Time.deltaTime);
+            
+            //If you press E then you gain speed because you drank some energy
+            if (Input.GetKey(KeyCode.E) && Cooldown == false)
+            {
+                speed = energySpeed;
+                StartCoroutine(waitAFewSeconds(6f));
+            }
         }
     }
 
-    //If you collide with the energy drink your speed gets faster for a little bit and your energy bar goes up you also get a 
-    //empty energy drink can in your inventory which you have to delever to a trash can.
+    // IEnumerator is a function that goes through each step
+    IEnumerator waitAFewSeconds(float seconds)
+    {
+        //wait for a couple of seconds
+        yield return new WaitForSeconds(2f);
+        speed = normalSpeed;
+        yield return new WaitForSeconds(4f);
+        Cooldown = true;
+    }
 }
