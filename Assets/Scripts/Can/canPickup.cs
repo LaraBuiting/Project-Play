f0c460ec1ce6cf60e1e26436207775145af30533
Muharrem Ceran <muharremceran@gmail.com>
2023-12-12 14:46:23 +0100

    Fixed Dirty can score

--- Assets/Scripts/Can/canPickup.cs
+++ Assets/Scripts/Can/canPickup.cs
@@ -1,14 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class canPickup : MonoBehaviour
 {
+    public TextMeshProUGUI MyCanAmountText;
+    private int CanAmount;
+
+    void Start()
+    {
+        CanAmount = 0;
+        MyCanAmountText.text = "Dirty cans: " + CanAmount;
+    }
+
     private void OnTriggerEnter2D(Collider2D Can)
     {
         if (Can.gameObject.CompareTag("Can"))
         {
+            CanAmount++;
             Destroy(Can.gameObject);
+            MyCanAmountText.text = "Dirty cans: " + CanAmount;
         }
     }
 }
